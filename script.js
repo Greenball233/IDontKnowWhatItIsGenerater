7e8027a64d8cdd38c895817cfc3cec326609fa85
Greenball233 <74454079+Greenball233@users.noreply.github.com>
2022-06-07 16:41:35 +0800

    QAQ

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ downloadButton.addEventListener("click", download);
 button.addEventListener('click', draw)
 
 function draw() {
-    if (document.fonts.status !== "loaded") return;
     ctx.fillStyle = "rgba(255, 255, 255, 1)";
     ctx.fillRect(0, 0, width, height);
     ctx.clearRect(leftPosX, avatarPos.y + avatarOffset + avatarRadius, width - 2 * leftPosX, height - avatarPos.y - avatarOffset - avatarRadius - bottomOffset);
